2cb167770b24667d55947734c3bed67a7a43b5af
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-07-06 17:02:29 -0400

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# password-gen
+# <Your-Project-Title>
+
+## Description
+
+User will use this app to generate and original and secure password. The main objective is to grant User security as they utilize their randomly generated password. 
+
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+Use this link to view project:
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+MIT License
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+
+## Features
+
+User can choose how long and complex their password can be by selecting:
+<li>charcter length;
+<li>upper / lowercase text;
+<li> symbols
+
+## How to Contribute
+
+N/A
+
+## Tests
+
+Utilized VS Code
--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+/*
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+- if user clicks the button:
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+- first prompt, ask user for length of password. must be between 8-128 only.
+- second prompt, ask user the character type: lowercase, uppercase, numeric, special characters.
+-- add confirm to validate the character types. 
+- display password on the page
+
+*/
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
