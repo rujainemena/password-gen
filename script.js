c354690c57c2c3f0f7e8c76c2cfe2303e4514a1e
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-07-11 11:27:49 -0400

    updated prompts and fixed for loop

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// defined the variables for the prompts
-var character = "!@#$%^&*()";
-var number = "1234567890";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+// variables I'm using for password
+var character = "!@#$%^&*()".split("");
+var number = "1234567890".split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-/*
-Pseudo Code based on criteria:
+// an array to collect the user's answers
+var savePassword = [];
 
-- first prompt (WINDOW.PROMPT), ask user for length of password. must be between 8-128 only.
--- alert user if character legnth is under 8 or over 128 with "invalid entry"
-
-- second prompt (WINDOW.CONFIRM - this is a boolean - true / false), ask user the character type: lowercase, uppercase, numeric, special characters.
--- add window.confirm  lowercase
--- add window.confirm  uppercase
--- add window.confirm  special characters
--- add window.confirm  numbers
-
-- must create variables to tell the computer what it is.
--- to save special characters
--- to save numbers
--- lowercase for A-Z
--- uppercase for A-Z
-
-- display password on the page
-
-*/
-
+// function to generate random password.
 function generatePassword() {
-  var savePassword = ""
-  var userLength = window.prompt("Enter your preferred password length.")
 
+  // prompt for the user to choose password length. 
+  var userLength = window.prompt("Enter your preferred password length. (Between 8 and 128 characters)")
+  if (userLength < 8 || userLength > 128) {
+    window.alert("Invalid Entry. Please enter a number between 8 and 128.")
+    return;
+  }
+  // confirm prompts for users to style their password. 
   if (userLength >= 8 && userLength <= 128) {
     var isLowerCase = window.confirm("Do you want to include lower case letters?");
     var isUpperCase = window.confirm("Do you want to include capital letters?");
@@ -40,42 +27,41 @@ function generatePassword() {
     var addCharacter = window.confirm("Do you want to include special characters?");
 
     if (isLowerCase === true) {
-      var randomIndex = Math.floor(Math.random() * lowerCase.length)
-      savePassword = savePassword + lowerCase[randomIndex]
+      // var randomIndex = Math.floor(Math.random() * lowerCase.length);
+      savePassword = savePassword.concat(lowerCase);
     }
 
     if (isUpperCase === true) {
-      var randomIndex = Math.floor(Math.random() * upperCase.length)
-      savePassword = savePassword + upperCase[randomIndex]
+      // var randomIndex = Math.floor(Math.random() * upperCase.length);
+      savePassword = savePassword.concat(upperCase);
     }
 
     if (addNumber === true) {
-      var randomIndex = Math.floor(Math.random() * number.length)
-      savePassword = savePassword + number[randomIndex]
+      // var randomIndex = Math.floor(Math.random() * number.length);
+      savePassword = savePassword.concat(addNumber);
     }
 
     if (addCharacter === true) {
-      var randomIndex = Math.floor(Math.random() * character.length)
-      savePassword = savePassword + character[randomIndex]
+      // var randomIndex = Math.floor(Math.random() * character.length);
+      savePassword = savePassword.concat(addCharacter);
     }
 
     if (isLowerCase === false && isUpperCase === false && addCharacter === false && addNumber === false) {
-      window.alert("You must choose at least two character types.")
+      window.alert("Please try again.");
     }
-
-  } else {
-    window.alert("Invalid Entry")
   }
 
-for (let i = 0; i < userLength; i++) {
-  
-}
-  return savePassword;
+  // variable to store final password
+  var finalPass = "";
+  for (let i = 0; i < userLength; i++) {
+    finalPass = finalPass + savePassword[Math.floor(Math.random() * savePassword.length)];
+  }
+  return finalPass;
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); 
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
